4093af33c6b847e2e59d24375b36069aea37dd61
vlopez <victorl_222@hotmail.com>
2022-05-25 14:03:28 -0300

    solution user and pass sesion

--- core/views.py
+++ core/views.py
@@ -90,10 +90,12 @@ def poweroff(request,id_equipo):
 
 def mem_pro_consum(id_equipo):
         equipo=Equipo.objects.get(pk=id_equipo)
-        admin = "\'" +equipo.user_admin+"\'"
-        print(type(admin))
+        # admin = "\'" +equipo.user_admin+"\'"
+        # print(type(admin))
+        admin=equipo.user_admin.format()
+        passw=equipo.passwordadmin.format()
         # session = winrm.Session(equipo.direction, auth=(equipo.user_admin,equipo.passwordadmin),transport='ntlm')
-        session = winrm.Session(equipo.direction, auth=('administrador','AMEC4m3c1962'),transport='ntlm')
+        session = winrm.Session(equipo.direction, auth=(admin,passw),transport='ntlm')
         resultmemory = session.run_ps("wmic OS get FreePhysicalMemory")
         datasub=resultmemory.std_out.decode('UTF-8')
         datalist = [int(i) for i in datasub.split() if i.isdigit()]
