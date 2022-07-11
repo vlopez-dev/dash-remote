b5b17a33b395d8c63be8d42899e40117294babae
Victor <victorl_222@hotmail.com>
2022-07-11 16:17:40 -0300

    form signup email

---
+++ registration/forms.py
@@ -0,0 +1,16 @@
+from dataclasses import field
+from pyexpat import model
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserCreationFormWithEmail(UserCreationForm):
+    email=forms.EmailField(required=True,help_text="Requerido .254 carácteres como máximo y debe ser un email válido.")
+    
+    
+    
+    
+    class Meta:
+        model=User
+        fields=("username","email","password1","password1")
--- registration/views.py
+++ registration/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-
+from .forms import UserCreationFormWithEmail
 
 # Create your views here.
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
