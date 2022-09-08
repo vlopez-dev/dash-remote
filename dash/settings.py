6c456b5c8a8e7e64068a83f19fba33cc6f65def1
Victor <victorl_222@hotmail.com>
2022-09-08 17:31:54 -0300

    .

--- dash/settings.py
+++ dash/settings.py
@@ -152,10 +152,10 @@ LOGOUT_REDIRECT_URL='login'
 
 
 
-EMAIL_HOST = config('EMAIL_HOST', default='192.168.1.1')
+EMAIL_HOST = config('EMAIL_HOST', default='')
 EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='@prueba!1962')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='prueba@amec.com.uy')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
 
